2b363351bb267b99ce2ff8137cfe3d4d3614608c
GabrielCarneiro098 <gabrielcarneiro098gc@gmail.com>
2023-06-16 00:46:32 -0300

    first commit

---
+++ atividade1.js
@@ -0,0 +1,5 @@
+const nomes = ["Gabriel", "Julia", "Caio", "Ana"]
+
+for (let index = 0; index < nomes.length; index++) {
+    console.log(nomes[index]) 
+}
---
+++ atividade3.js
@@ -0,0 +1,14 @@
+const frutas = ["maçã", "kiwi", "melão"]
+console.log(frutas)
+
+frutas.push("uva")
+console.log(frutas)
+
+frutas.pop()
+console.log(frutas)
+
+frutas.unshift("tomate")
+console.log(frutas)
+
+frutas.shift()
+console.log(frutas)
